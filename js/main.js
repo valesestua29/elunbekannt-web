a044d5d9bd8ccf48d760a22c7846aebdd29b58a1
Valeria Sestua <valeriasestua29@gmail.com>
2024-10-16 21:06:40 -0300

    carrusel modif

--- js/main.js
+++ js/main.js
@@ -22,14 +22,48 @@ var swiper = new Swiper (".slide-content", {
     spaceBetween: 50,
     slidesPerGroup: 3,
     loop:true,
-    loopFillGroupWithBlank: true,
+    centerSlide: 'true',
+    gragCursor: 'true',
     pagination: {
         el: ".swiper-pagination",
         clickable: true,
+        dynamicBullets: true,
     },
     navigation: {
         nextEl: ".swiper-button-next",
         prevEl: ".swiper-button-prev",
-    }
+    },
 
+
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+            spaceBetween: 20,
+        },
+        320: {
+            slidesPerView: 1,
+            spaceBetween: 20,
+        },
+        425: {
+            slidesPerView: 1,
+            spaceBetween: 10,
+        },
+        768: {
+            slidesPerView: 2,
+            spaceBetween: 50,
+        },
+        1024: {
+            slidesPerView: 3,
+            spaceBetween: 20,
+        },
+        1440: {
+            slidesPerView: 3,
+            spaceBetween: 20,
+        },
+        2560: {
+            slidesPerView: 3,
+            spaceBetween: 25,
+        },
+    },
 });
+
